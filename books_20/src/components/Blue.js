b640df676821d8db2a63ade082fe209c30e5432f
windowPC <drivespublic@gmail.com>
2022-05-22 15:04:46 +0900

    commit before eject

---
+++ books_20/src/components/Blue.js
@@ -0,0 +1,6 @@
+import "./Blue.css";
+const Blue = () => {
+  return <div className="blue">blue</div>;
+};
+
+export default Blue;
---
+++ books_20/src/components/Menu.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <ul>
+      <li>
+        <Link to={"/red"}>RED</Link>
+      </li>
+      <li>
+        <Link to={"/blue"}>BLUE</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Menu;

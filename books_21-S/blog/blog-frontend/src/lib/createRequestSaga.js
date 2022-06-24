7e826a6f50c7a35d2e3078d38f031d07650b7bda
windowPC <drivespublic@gmail.com>
2022-06-24 09:10:25 +0900

    redux -ing

--- books_21-S/blog/blog-frontend/src/lib/createRequestSaga.js
+++ books_21-S/blog/blog-frontend/src/lib/createRequestSaga.js
@@ -1,6 +1,12 @@
 import { call, put } from 'redux-saga/effects';
 import { finishLoading, startLoading } from '../modules/loading';
 
+export const createRequestActionTypes = (type) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return [type, SUCCESS, FAILURE];
+};
+
 export default function createRequestSaga(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
@@ -8,7 +14,9 @@ export default function createRequestSaga(type, request) {
   return function* (action) {
     yield put(startLoading(type));
     try {
-      const response = yield call(request, action.payload);
+      const response = yield call(request, action.payload); // URL, Parameter
+      console.log('redux에서 auth로 왜 받을 수 있는지 확인중');
+      console.log('response - ', response);
       yield put({
         // dispatch
         type: SUCCESS,
--- books_21-S/blog/blog-frontend/src/modules/auth.js
+++ books_21-S/blog/blog-frontend/src/modules/auth.js
@@ -1,17 +1,20 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as authAPI from '../lib/api/auth';
+import { takeLatest } from 'redux-saga/effects';
 
 // 액션
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-const REGISTER = 'auth/REGISTER';
-const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
-const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] =
+  createRequestActionTypes('auth/REGISTER');
 
-const LOGIN = 'auth/LOGIN';
-const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
-const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] =
+  createRequestActionTypes('auth/LOGIN');
 
 // 액션 생성 함수
 export const changeField = createAction(
@@ -22,7 +25,24 @@ export const changeField = createAction(
     value, // payload
   }),
 );
+
 export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+export const register = createAction(REGISTER, ({ username, password }) => ({
+  username,
+  password,
+}));
+export const login = createAction(LOGIN, ({ username, password }) => ({
+  username,
+  password,
+}));
+
+// SAGA 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+  yield takeLatest(REGISTER, registerSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
 
 const initState = {
   register: {
@@ -34,6 +54,8 @@ const initState = {
     username: '',
     password: '',
   },
+  auth: null,
+  authError: null,
 };
 
 const auth = handleActions(
@@ -46,6 +68,26 @@ const auth = handleActions(
     [INITIALIZE_FORM]: (state, { payload: form }) => ({
       ...state,
       [form]: initState[form],
+    }),
+    [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
+      // 왜 auth로 받을까
+      ...state,
+      authError: null,
+      auth,
+    }),
+    [REGISTER_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error,
+    }),
+    [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+      // 왜 auth로 받을까
+      ...state,
+      authError: null,
+      auth,
+    }),
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error,
     }),
   },
   initState,
